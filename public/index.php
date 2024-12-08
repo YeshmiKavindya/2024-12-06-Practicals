8945aead1ed8ee2c78791271a1f04a806fc1a05b
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-08 06:05:41 +0000

    edit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020ASP87</h1><br><br>
+
+    <a href="gettable.php">Gettable</a><br>
+    
+    <a href="myfun.php">MyFunction</a><br>
+
+
+    
+</body>
+</html>
