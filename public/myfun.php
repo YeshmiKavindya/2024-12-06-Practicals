b07ccceea4e94f0aa6a08bf2e87d1734bcae0ab5
Yeshmi Kavindya <139144140+YeshmiKavindya@users.noreply.github.com>
2024-12-08 06:06:14 +0000

    edit

---
+++ public/myfun.php
@@ -0,0 +1,189 @@
+
+
+<?php
+//get the database connection file
+require_once 'dbconf.php';
+/*function PrintTable($tableName,$connect){
+try{
+	//Query
+	$sql = "SELECT * FROM $tableName";
+
+	//execute the query (call variable,query)
+	$result = mysqli_query($connect,$sql);
+
+	if (mysqli_num_rows($result)>0) {
+		//fetch the data from rows
+
+		echo "<table border=1> ";
+
+	$col = mysqli_fetch_fields($result);
+	//print the column
+	echo "<tr>";
+	foreach ($col as $value) {
+		//return an object
+		//print_r($value)
+			echo "<td>".$value->name."</td>";
+		}
+		echo "</tr>";
+		while($row = mysqli_fetch_assoc($result)){
+			//print the data in table format
+
+		echo "<tr>";
+		foreach ($row as $key => $value) {
+			echo "<td>$value</td>";
+		}
+		echo "</tr>";
+		}
+		echo "</table>";
+	} 
+	else{
+		echo "No results"; //table is empty
+	}
+
+	}
+
+
+catch(Exception $e){
+	die($e->getMessage());
+}
+}*/
+ //GET DATA FROM DB
+ function student($connect){
+            try{
+            
+                $sql = "SELECT student_id, name FROM  students";
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+
+                    echo "<td> View details </td>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){ 
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    $student_id=$row['student_id'];
+                    //query string
+                    echo "<td><a href='printtable.php? student_id=$student_id '> View </a> </td>";
+                    echo "</tr>";
+                    
+                    }
+
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }
+
+
+
+         function Studentdetails($student_id,$connect){
+            try{
+            
+                $sql = "SELECT * FROM  students where  student_id = student_id ";
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){
+                        
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                    }
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }
+
+       /* 
+	Join the tables
+       function Jointable($EMP_ID,$connect){
+            try{
+            
+                $sql = "SELECT * FROM students INNER JOIN enrollments ON students.student_id = enrollments.student_id;
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){
+                        
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                    }
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }*/
+?>
